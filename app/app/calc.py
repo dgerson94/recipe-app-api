998838ce096f64767b3360561d5d7d58caf65418
Dov Gerson <dgerson94@gmail.com>
2022-07-20 11:27:22 +0300

    5

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,11 +3,12 @@ Caculator functions
 """
 
 
-def add (x,y):
+def add(x, y):
     """Add x and Y and return result"""
     return x + y
 
-def subtract (x,y):
-    """Subtract x from y  and return result"""
+def subtract(x, y):
+    """Subtract x from y and return result"""
     
-    return y-x
+    return y - x
+    
